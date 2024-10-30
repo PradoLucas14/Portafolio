81a2b493e38d39bcebf2d4f8ff560959520b69aa
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-10-30 16:48:41 -0300

    se realizaron modificaciones en la hoja de estilos del componente about

--- vite-project/src/pages/home/About.jsx
+++ vite-project/src/pages/home/About.jsx
@@ -3,21 +3,31 @@ import vectorImage from '../../assets/Home/vector1.webp'; // Importa el vector d
 import "./About.css"
 
 function About() {
-  return (
-    <div className="about-container">
-      <div className="column column-vector">
-        <div className="imgContainer">
-            <img src={vectorImage} alt="Vector" className="vector-image" />
+    return (
+        <div className="about-container">
+            <div className="column column-vector">
+                <div className="imgContainer">
+                    <img src={vectorImage} alt="Vector" className="vector-image" />
+                </div>
+            </div>
+            <div className="column column-text">
+                <div className="texContainer">
+                    <h2>Conoceme:</h2>
+                    <p>
+                        Soy un desarrollador web con un año de experiencia y una pasión inagotable por resolver problemas a través de tecnología innovadora. 
+                        He creado aplicaciones intuitivas y dinámicas que mejoran la experiencia del usuario, y domino herramientas modernas que optimizan 
+                        l rendimiento y la estética. Mi meta es llevar cada proyecto al siguiente nivel con un enfoque ágil, creativo y orientado a resultados. 
+                        ¡Listo para impulsar el éxito de tu equipo con código de calidad y soluciones frescas!
+                    </p>
+                    <div className='redesAbout'>
+                        <div className="red"></div>
+                        <div className="red"></div>
+                        <div className="red"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="column column-text">
-        <p>
-          Aquí va el texto que describe la página o el propósito de tu sitio.
-          Puedes agregar más contenido o ajustar el estilo según tus necesidades.
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default About;
